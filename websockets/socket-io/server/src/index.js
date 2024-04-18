d2e253e2193e4ef945e2bd64e67cae4181fcf56e
vishal <vishalmahesh.study@gmail.com>
2024-04-18 13:34:17 +0530

    a simple socket-io web server

--- websockets/socket-io/server/src/index.js
+++ websockets/socket-io/server/src/index.js
@@ -1,16 +1,28 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
-import { Socket } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 
-const io = Socket(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log("SOCKET: ", socket);
   console.log("A user connected at:", new Date());
+  console.log("Number of users: ", io.engine.clientsCount);
+
+  socket.on("chat", (payload) => {
+    console.log("PAYLOAD RECEIVED:", payload);
+    io.emit("chat", payload);
+  });
+
+  socket.on("error", (err) => {
+    console.log("ERROR: ", err);
+  });
 });
 
 const PORT = 3000;
