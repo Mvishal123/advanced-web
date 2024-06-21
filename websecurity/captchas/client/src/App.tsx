141283e5760614dda509a101e9f3c2c9615abca6
vishal <vishalmahesh.study@gmail.com>
2024-06-21 13:52:39 +0530

    add captcha token verification in server

--- websecurity/captchas/client/src/App.tsx
+++ websecurity/captchas/client/src/App.tsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import { Turnstile } from "@marsidev/react-turnstile";
 
+
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
   const [isCaptcha, setIsCaptcha] = useState<boolean>(false);
+  const [token, setToken] = useState<string>("");
+
+  const onSubmithandler = async () => {
+    const res = await axios.post("http://localhost:3000/registeruser", {
+      token,
+    });
+
+    console.log(res.data);
+  };
+
+  console.log({ token });
   console.log(import.meta.env.VITE_CAPTCHA_SITE_KEY);
   return (
     <main>
@@ -28,14 +41,17 @@ const App = () => {
             disabled={!isCaptcha}
           />
         </div>
-        <button disabled={!isCaptcha}>Register</button>
+        <button disabled={!isCaptcha} onClick={onSubmithandler}>Register</button>
       </div>
       {/* <div className="cf-turnstile" data-sitekey={import.meta.env.VITE_CAPTCHA_SITE_KEY} data-callback="javascriptCallback"
         onSuccess={() => setIsCaptcha(true)}
       ></div> */}
       <Turnstile
         siteKey={import.meta.env.VITE_CAPTCHA_SITE_KEY}
-        onSuccess={() => setIsCaptcha(true)}
+        onSuccess={(token) => {
+          setToken(token);
+          setIsCaptcha(true);
+        }}
       />
     </main>
   );
